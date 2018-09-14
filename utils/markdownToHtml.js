8a4b261ac2ae9fdcd49696eb451e782792f0affe
genluo <17051298278@163.com>
2018-09-14 11:17:40 +0800

    fixed bug

--- utils/markdownToHtml.js
+++ utils/markdownToHtml.js
@@ -14,8 +14,7 @@ const isBlock = str => {
 class Converter {
   // 转化成html标记语言
   toHtml(md) {
-    return this.toBlock(md)
-
+    return this.blockToHtml(this.toBlock(md))
   }
 
   // 渲染生成富文本，用于添加添加样式
@@ -31,16 +30,14 @@ class Converter {
     for(let i = 0; i < mdarr.length; i++) {
       // 判断当前是不是块级元素，如果是的话判他的上一个是不是块级元素，不是的话进行合并
       // 处理``` 格式
+      
       if(identification) {
         blockMD[blockMD.length-1] += '\n' + mdarr[i]
+        continue;
       }
-      if(/^( {0,3}`{3,3})/g.test(mdarr[i]) && !identification) {
-        identification = true;
+      if(/^( {0,3}`{3,3})/g.test(mdarr[i])) {
+        identification = !identification;
       }
-      if(/^( {0,3}`{3,3})/g.test(mdarr[i]) && identification) {
-        identification = false;
-      }
-
       if(i>0 && !isBlock(mdarr[i])) {
 
         if(!isBlock(mdarr[i-1])) {
@@ -57,32 +54,43 @@ class Converter {
   blockToHtml(blockArr) {
     const blockHtmlArr = [];
     for(const item of blockArr) {
-      let blockIdentifident = item.match(regex)[0].trim();
+      console.log(item.match(regex))
+      let blockIdentifident = item.match(regex) && item.match(regex)[0].trim();
       
       // #
       if(/#/g.test(blockIdentifident)) {
-        blockHtmlArr.push(`<h${blockIdentifident.length}>${item}</h${blockIdentifident.length}>`)
+        blockHtmlArr.push(`<h${blockIdentifident.length}>${item.replace(/^( {0,3}#+ )/g,'')}</h${blockIdentifident.length}>`)
+        continue
       }
       // * | -
       if(/\*|\-/g.test(blockIdentifident)) {
-        blockHtmlArr.push(`<li>${item}</li>`)
+        blockHtmlArr.push(`<li>${item.replace(/^( {0,}[\*|\-] )/g,'')}</li>`)
+        continue
       }
       // ---
       if(/-+/g.test(blockIdentifident)) {
         blockHtmlArr.push(`<dl></dl>`)
+        continue
       }
       // ```
       if(/```/g.test(blockIdentifident)) {
-        blockHtmlArr.push(`<div class="code">${item}</div>`)
+        blockHtmlArr.push(`<div class="code">${item.replace(/^( {0,3}```)/g, '')}</div>`)
+        continue
       }
+      // 行内标签
+      blockHtmlArr.push(`<p>${item}</p>`)
     }
     return blockHtmlArr;
   }
 
   // 处理行行内标签
+  inLineHtml(md) {
+    
+  }
+
 }
 
 
 const converter = new Converter();
 
-console.log(converter.toHtml('# xianan \n x  ### genge \n xianzai \n``` \n xianzaijing \n xian  sad fsa f \n```' ))
+console.log(converter.toHtml('# xianan \n x  **###** genge \n xianzai \n``` \n xianzaijing \n xian  \n```' ))
