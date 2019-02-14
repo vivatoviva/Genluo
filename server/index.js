6844102e3afea68b4d3678c4bd171d339cf31210
vivatoviva <1461304646@qq.com>
2019-02-14 23:49:30 +0800

    test

--- server/index.js
+++ server/index.js
@@ -19,27 +19,25 @@ app.prepare()
     server.keys = ['some secret hurr'];
     server.use(KoaSession(sessionConfig, server));
     server.use(koaBody());
-    // 中间件插件
+    // use middleware
     server.use(koaLogger);
     server.use(koaStatus);
-    // 挂载数据库对象
+    // Mount the database
     server.use(async (ctx, next) => {
       ctx.mysql = mysql;
       await next();
     });
-    // 添加相关路由
+    // add router map
     routers.map(item => server.use(item.routes()));
-    // 静态资源渲染
+    // static resource
     server.use(async (ctx) => {
       await handle(ctx.req, ctx.res);
       ctx.respond = false;
     });
-    // 最后收尾返回404页面
     server.use(async (ctx, next) => {
       ctx.res.statusCode = 200;
       await next();
     });
-
     server.listen(port, (err) => {
       if (err) throw err;
       console.info(`> Ready on http://localhost:${port}`);
--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
-const renderRouter = require('./render');
-const clientRouter = require('./client');
-const adminRouter = require('./admin');
+const renderRouter = require('./render'); // 后端映射到前端的相关路由
+const clientRouter = require('./client'); // 客户端的相关路由
+const adminRouter = require('./admin'); // 管理端的相关路由
 
 module.exports = [
   clientRouter,
